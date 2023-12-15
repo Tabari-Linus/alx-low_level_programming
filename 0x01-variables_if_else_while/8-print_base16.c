27cddfa6ece7b1fe434d3d088fc310e12e7744fe
linus <leetamparega>
2023-12-15 10:17:49 -0800

    task 8 complete

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 char letas;
 int nums;
 
-for (nums = 0; nums <= 9; nums++)
+for (nums = '0'; nums <= '9'; nums++)
 putchar(nums);
 for (letas = 'a'; letas <= 'f'; letas++)
 putchar(letas);
