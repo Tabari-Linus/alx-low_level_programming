b37858489cc2cb45e2577383a6a006241a22bc4a
linus <leetamparega>
2023-12-15 10:15:15 -0800

    task 8 complete

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letas;
+int nums;
+
+for (nums = 0; nums <= 9; num++)
+putchar(nums);
+for (letas = 'a'; letas <= 'f'; letas++)
+putchar(letas);
+putchar('\n');
+return (0);
+}
