5d7932ebada2e1df9ae18bc34a2e1c4cdb5b8ab3
linus <leetamparega>
2023-12-14 16:50:00 -0800

    all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-  int int_type;
-  long int l_i_type;
-  long long int l_l_i_type;
-  char char_type;
-  float float_type;
+int int_type;
+long int l_i_type;
+long long int l_l_i_type;
+char char_type;
+float float_type;
 
-  printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char_type));
-  printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int_type));
-  printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l_i_type));
-  printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l_l_i_type));
-  printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float_type));
-  return(0);
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char_type));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int_type));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l_i_type));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l_l_i_type));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float_type));
+return (0);
 }
