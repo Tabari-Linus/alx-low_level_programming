59ea8e07c67b3beb5bfbd44bf80e4a9f26e24590
linus <leetamparega>
2023-12-15 10:08:24 -0800

    task 7 complete

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char leta;
+
+for (leta = 'z'; leta >= 'a'; leta--)
+putchar(leta);
+putchar('\n');
+return (0);
+}
