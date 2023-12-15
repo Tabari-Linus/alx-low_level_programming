e6fd3fdb5ce4fe05cc0665bd748ceacb0c016bdc
linus <leetamparega>
2023-12-15 06:47:19 -0800

    task 2 complete

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char leta;
+
+for leta = 'a'; leta <= 'z'; leta ++)
+putchar(leta);
+putchar("\n");
+return (0);
+}
