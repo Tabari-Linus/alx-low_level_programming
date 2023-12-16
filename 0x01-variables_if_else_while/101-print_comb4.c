47ec4f116ebc15140e16bd8f9b4704fb83df7829
linus <leetamparega>
2023-12-15 17:50:17 -0800

    task 11 complete

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num, a, b;
+
+for (num = '0'; num < '9'; num++)
+{
+for (a = num + 1; a <= '9'; a++)
+{
+for (b = a + 1; b <= '9'; b++)
+{
+if ((a != num) != b)
+{
+putchar(num);
+putchar(a);
+putchar(b);
+if (num == '7' && a == '8')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
