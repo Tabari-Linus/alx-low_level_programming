41860f9c9281826074248dcc2f69f6e09007000f
linus <leetamparega>
2023-12-20 10:23:07 -0800

    setting up for 0x02-functions_nested_loops and pushing task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+int _putchar(char a);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int a);
+int _isalpha(int a);
+int print_sign(int a);
+int _abs(int a);
+int print_last_digit(int a);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int a);
+void print_times_table(int a);
+void putformat(int);
