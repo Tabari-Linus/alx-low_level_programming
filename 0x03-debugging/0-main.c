a8fa5a161b9a0ecf808d851a5dcd386b71272d1f
linus <leetamparega>
2023-12-22 16:05:28 -0800

    all task complete

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int a;
+
+a = 0;
+positive_negative(a);
+
+return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+int a;
+
+printf("Infinite loop incoming :(\n");
+
+a = 0;
+
+/* while (a < 10)*/
+/* {*/
+/*
+/* }*/
+
+printf("Infinite loop avoided \\o/\n");
+
+return (0);
+}
