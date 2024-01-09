17592da4769918f7f8c24e49028f4b3862a5328e
linus <leetamparega>
2024-01-09 08:01:55 -0800

    all done

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - a function that prints the if the two diagonals
+ * of a square matrix of integers.
+ * @a: a square matrix of integers
+ * @size: an input integer with the size of matrix
+ * Return: Nothing
+ */
+void print_diagsums(int *a, int size)
+{
+int t = 0, max = sixe * size, sum1 = 0, sum2 =0;
+
+for (; t < max; t += size + 1)
+sum1 += a[t];
+
+for (t = size - 1; t < max - 1; t += size - 1)
+sum2 += a[t];
+
+printf("%d, %d\n", sum1, sum2);
+}
