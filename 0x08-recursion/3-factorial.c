8d5278be16c3ff45a7f309456b52020802c0ccd3
linus <leetamparega>
2024-01-10 15:09:06 -0800

    task 3 error fixes

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,11 +5,11 @@
  * @a: An input integer
  * Return: The factorial of a
  */
-int factorial(int n)
+int factorial(int a)
 {
 if (a < 0)
 return (-1);
 if (a <= 1)
 return (1);
-return ( a * factorial(n -1));
+return ( a * factorial(a -1));
 }
