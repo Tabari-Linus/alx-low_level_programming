6e4e43fc99bff11c307468b58cba50fade2b07f6
linus <leetamparega>
2024-01-13 16:48:11 -0800

    SOME

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,11 @@
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strncpy(char *dest, char *src, int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+unsigned int _strspn(char *s, char *accept);
