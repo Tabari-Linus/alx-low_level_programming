cfe15c0120e9febbd128449d6f6345f0909304cc
linus <leetamparega>
2024-01-24 18:19:44 -0800

    errors corrcetion

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,14 +6,14 @@
  * @array: A pointer to array
  * @size: Size of the array
  * @cmp: cmp(function) to compare the elements throw array
- * Return: The index of the first element for which the cmp function
+ * Return: The index of the first element for witch the cmp function
  * does not return 0; -1 if no elements matches or size <= 0.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i = 0;
 
-if (cmp == NULL || array ++ NULL)
+if (cmp == NULL || array == NULL)
 return (-1);
 
 if (size <= 0)
