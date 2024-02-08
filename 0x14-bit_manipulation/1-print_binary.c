3ffe2893fca36340deea7ad6a9fe687b329b03b6
linus <leetamparega>
2024-02-08 11:25:54 -0800

    task 1 to 3 complete

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary - A function that prints a binary number without % or /
+ * @n: The number to be printed
+ * Return: Nothing
+ */
+void print_binary(unsigned long int)
+{
+  if (n > 1)
+    print_binary(n >> 1);
+
+  _putchar((n & 1) + '0');
+}
